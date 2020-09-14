dd3ed980432be9e5e1c3f5664f38e6f971f5c192
milochadam <miloch.adam@gmail.com>
2020-09-14 17:26:09 +0200

    Updated headers

--- scrapers/cambridge.py
+++ scrapers/cambridge.py
@@ -1,15 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+
 from scrapers.scraper import Scraper
 
-
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+    'Accept': 'application/json',
+    'Accept-Language': 'pl,en;q=0.5',
+    'Referer': 'https://dictionary.cambridge.org/pl/dictionary/english/test',
+    'AMP-Same-Origin': 'true',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+}
 class CambrScraper(Scraper):
     WEBSITE = "https://dictionary.cambridge.org/pl/dictionary/english/"
 
     def scrape_data(self, word):
-        print("\nScraping from cambridge dictionary: {}".format(word))
+        print("Scraping from cambridge dictionary: {}".format(word))
         website = f'{self.WEBSITE}{word}'
-        request = requests.get(website).text
+        request = requests.get(website, headers=headers).text
         soup = BeautifulSoup(request, 'lxml')
         word_from_site = soup.find('div', class_='pos-header dpos-h').find('span', class_='hw dhw').text
         try:
